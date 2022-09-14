0b124010269cea2da4f708e5ddde80df79b634b3
xylaron <aldo6701@gmail.com>
2022-09-14 10:56:22 +0800

    combine with UserAttr and add value pack switch

--- src/Functions/GrindCalc/GrindInput/UserInput.jsx
+++ src/Functions/GrindCalc/GrindInput/UserInput.jsx
@@ -1,10 +1,10 @@
 /* eslint-disable no-loop-func */
-import React, { useState, useContext } from "react";
+import React, { useState, useContext, useEffect } from "react";
 import Button from "Components/Button";
 import { InputContext } from "../GrindOutput/Table";
 import { PlusIcon } from "@heroicons/react/solid";
+import { Switch } from "@headlessui/react";
 import { sycraia } from "database";
-import UserAttr from "./UserAttr";
 
 const UserInput = ({ toggleModal }) => {
   let obj = {};
@@ -19,6 +19,12 @@ const UserInput = ({ toggleModal }) => {
   }
 
   const [userInputData, setUserInputData] = useState(obj);
+  const [hasValuePack, setHasValuePack] = useState(0);
+
+  useEffect(() => {
+    console.log("button status: ", hasValuePack)
+  }, [hasValuePack])
+  
 
   const handleChange = (event) => {
     const name = event.target.name;
@@ -34,6 +40,7 @@ const UserInput = ({ toggleModal }) => {
     const itemInputConv = itemInput.map((str) => {
       return Number(str);
     });
+    // const fullUserInput = 
     console.log(userInputData);
     updateInputData(itemInputConv);
   };
@@ -69,9 +76,42 @@ const UserInput = ({ toggleModal }) => {
           </tbody>
         </table>
       </div>
-      <UserAttr id="family-fame" handleChange={handleChange}>
-        Family Fame
-      </UserAttr>
+      <div className="divbox mt-5 flex">
+        <div className="">
+          <label className="label">
+            <span className="label-text font-bold">Family Fame</span>
+          </label>
+          <div className="flex items-center justify-center">
+            <input
+              type="number"
+              name="family-fame"
+              className="input w-20 rounded-lg border-zinc-700 bg-zinc-900 px-2 py-1 text-center text-lg font-bold"
+              placeholder="0"
+              onChange={handleChange}
+            />
+          </div>
+        </div>
+        <div className="ml-2">
+          <label className="label">
+            <span className="label-text font-bold">Value Pack?</span>
+          </label>
+          <div className="flex h-[48px] w-[80px] items-center justify-center">
+            <Switch
+              checked={hasValuePack}
+              onChange={setHasValuePack}
+              className={`${hasValuePack == 1 ? "bg-green-500" : "bg-zinc-400"}
+          relative inline-flex h-8 w-16 shrink-0 cursor-pointer rounded-full border-2 border-transparent transition-colors duration-200 ease-in-out focus:outline-none focus-visible:ring-2  focus-visible:ring-white focus-visible:ring-opacity-75`}
+            >
+              <span className="sr-only">Use setting</span>
+              <span
+                aria-hidden="true"
+                className={`${hasValuePack == 1 ? "translate-x-8" : "translate-x-0"}
+            pointer-events-none inline-block h-7 w-7 transform rounded-full bg-white shadow-lg ring-0 transition duration-200 ease-in-out`}
+              />
+            </Switch>
+          </div>
+        </div>
+      </div>
       <div className="mt-5">
         <Button type="submit" onClick={toggleModal} disabled={isDisabled} color="green" padding="3">
           <PlusIcon className="h-6 w-6" />
